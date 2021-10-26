02124511a2df2e6fed03527c94b4b5891d14727e
florenciaferraiolo <93006633+florenciaferraiolo@users.noreply.github.com>
2021-10-26 14:00:17 -0300

    Update ejercicio2ConFuncion.swift

--- ejercicio2ConFuncion.swift
+++ ejercicio2ConFuncion.swift
@@ -1,17 +1,1 @@
-import Foundation
-
-    func searchLowerAndGreater(numbers:[Int], number:Int){
-    var numberLow=0
-    var numberGreat=0
-    for (index, n) in numbers.enumerated(){
-        if(n==number){
-            numberLow = index
-            numberGreat=(numbers.count-1)-index}
-    }
-   print("There are \(numberLow) numbers lower and \(numberGreat) greater than \(number)")
-    }
-
-searchLowerAndGreater(numbers:[110, 20, 100, 0, 200], number:100)
-
-
 
